ca0ca6fc3a474e7e47b4ce91f176ea5f3dc56033
Parthiv-M <parthiv.menon@learner.manipal.edu>
2023-01-03 14:01:51 +0530

    complete artwork section and inlcude data file

---
+++ src/utils/constants.ts
@@ -0,0 +1,24 @@
+const artworks: string[] = [
+    "https://ik.imagekit.io/vk9h2knt4xe/Artwork/Snapchat-652623745-01_pHMvJ2oDb.jpeg",
+    "https://ik.imagekit.io/vk9h2knt4xe/Artwork/IMG_20211130_195628_361_JI6AWXBkFty.jpg",
+    "https://ik.imagekit.io/vk9h2knt4xe/Artwork/IMG_20211015_174154_760_ag4jzorT4cIu.jpg",
+    "https://ik.imagekit.io/vk9h2knt4xe/Artwork/IMG_20211015_174154_745_EMa_ZrxgFmT.jpg",
+    "https://ik.imagekit.io/vk9h2knt4xe/Artwork/IMG_20210713_112518_061_q45KxNCvKZdJ.jpg",
+    "https://ik.imagekit.io/vk9h2knt4xe/Artwork/IMG_20210713_112518_046_rZL3KB7yrM_.jpg",
+    "https://ik.imagekit.io/vk9h2knt4xe/Artwork/IMG_20210707_123455_424_0EvhmkVl4Fr2.jpg",
+    "https://ik.imagekit.io/vk9h2knt4xe/Artwork/IMG_20210707_123455_388_8lMtHB5tzlee.jpg",
+    "https://ik.imagekit.io/vk9h2knt4xe/Artwork/IMG_20210415_183604_391_qQfTwsqJXPh.jpg",
+    "https://ik.imagekit.io/vk9h2knt4xe/Artwork/IMG_20200523_131016_271_SDhaCk47M4q.jpg",
+    "https://ik.imagekit.io/vk9h2knt4xe/Artwork/IMG_20200511_111231_320_z_RsBj6dMSPX.jpg",
+    "https://ik.imagekit.io/vk9h2knt4xe/Artwork/IMG_20200414_215104_590_wip8eYmpC4VZ.jpg",
+    "https://ik.imagekit.io/vk9h2knt4xe/Artwork/IMG_20200330_115801_753_HAQQoqhkge9.jpg",
+    "https://ik.imagekit.io/vk9h2knt4xe/Artwork/IMG_20200323_192657_389_c1dot70rfG1.jpg",
+    "https://ik.imagekit.io/vk9h2knt4xe/Artwork/IMG_20191213_222533_844_xbse7NeUncP.jpg",
+    "https://ik.imagekit.io/vk9h2knt4xe/Artwork/IMG_20191212_212354_242_f_LR3vEnLavy.jpg",
+    "https://ik.imagekit.io/vk9h2knt4xe/Artwork/IMG_20191211_145206_076_Jp1z7lidZKc.jpg",
+    "https://ik.imagekit.io/vk9h2knt4xe/Artwork/IMG_20191125_195434_133_C49Oy1S-lfp.jpg",
+    "https://ik.imagekit.io/vk9h2knt4xe/Artwork/IMG_20190818_170712_157_gsmOUaNIdXHX.jpg",
+    "https://ik.imagekit.io/vk9h2knt4xe/Artwork/IMG_20190805_104018_592_sjardco23tl.jpg"
+]
+
+export default artworks;
